eb1e8965e3499beaab42b0e60d9f62f8465c91f3
Aditya raj <adityaraj4868@gmail.com>
2025-12-31 20:43:47 +0530

    Implement Vehicle and Bike classes with polymorphism

--- polymorphism/Test.java
+++ polymorphism/Test.java
@@ -1,2 +1,24 @@
 // Vehicle class
-class Vehicle 
+class Vehicle {
+    void run() {
+        System.out.println("Vehicle is running");
+    }
+}
+//Bike class
+class Bike extends Vehicle {
+    void run(){
+    System.out.println("Bike is running safely");
+    }
+}
+//Test class
+public class Test {
+    public static void main(String[] args){
+        Vehicle v;
+        v = new Vehicle();
+        v.run();
+
+        v = new Bike();
+        v.run();
+
+    }
+}
